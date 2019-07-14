81bcfa5ef306abda17d7c10f1710449d30df8a46
Jaylon Gowie <jaylongowie46@gmail.com>
2019-07-14 12:27:53 -0500

    Fixed Uptime Bug

--- src/TimeUtil.js
+++ src/TimeUtil.js
@@ -1,14 +1,14 @@
 class TimeUtil {
     static formatSec(sec) {
         if (!sec) return null
-        const seconds = sec
-        const minutes = Math.round(seconds / 60);
-        const hours = Math.round(minutes / 60);
-        const days = Math.round(hours / 24);
+        const seconds = sec;
+        const minutes = Math.floor(seconds / 60);
+        const hours = Math.floor(minutes / 60);
+        const days = Math.floor(hours / 24);
         const result = [];
-        if (days > 1) result.push( `${days}d`);
-        if (hours > 1) result.push(`${hours % 60}h`);
-        if (minutes > 1) result.push(`${minutes % 60}m`);
+        if (days >= 1) result.push( `${days}d`);
+        if (hours >= 1) result.push(`${hours % 24}h`);
+        if (minutes >= 1) result.push(`${minutes % 60}m`);
         result.push(`${seconds % 60}s`);
         return result.join(' ');
     } 
