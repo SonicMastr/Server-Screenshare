c4b106a45f11850b64bc48498910cf1a71d3f378
SonicMastr <jaylongowie46@gmail.com>
2019-08-28 21:27:29 -0500

    other shit

--- index.js
+++ index.js
@@ -1,2 +1,2 @@
 const Server = require('./src/server');
-new Server({ messageCacheLifetime: 1, messageSweepInterval: 300, shardCount: 2, totalShardCount: 2, fetchAllMembers: false });
+new Server({ messageCacheMaxSize: 1, messageCacheLifetime: 1, messageSweepInterval: 300, shardCount: 2, totalShardCount: 2, fetchAllMembers: false });
