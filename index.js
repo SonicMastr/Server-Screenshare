74a4070944d3af133bd4b32613c172883d3d1b1f
SonicMastr <jaylongowie46@gmail.com>
2019-08-28 21:21:59 -0500

    shard

--- index.js
+++ index.js
@@ -1,2 +1,2 @@
 const Server = require('./src/server');
-new Server({ messageCacheLifetime: 1, messageSweepInterval: 300 });
+new Server({ messageCacheLifetime: 1, messageSweepInterval: 300, shardCount: 2, totalShardCount: 2 });
