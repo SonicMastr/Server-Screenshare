cfa7513c09c94c6fe2ebf60d44ceed61b884221b
SonicMastr <jaylongowie46@gmail.com>
2019-08-29 00:08:12 -0500

    yes

--- index.js
+++ index.js
@@ -1,2 +1,2 @@
 const Server = require('./src/server');
-new Server({ messageCacheMaxSize: 1, messageCacheLifetime: 1, messageSweepInterval: 300, shardCount: 2, totalShardCount: 2, disableEvents:["VOICE_STATE_UPDATE", "TYPING_START"] });
+new Server({ messageCacheMaxSize: 1, messageCacheLifetime: 1, messageSweepInterval: 300, shardCount: 2, totalShardCount: 2, disableEvents:["VOICE_STATE_UPDATE", "TYPING_START", "MESSAGE_DELETE_BULK", "WEBHOOKS_UPDATE", "PRESENCE_UPDATE"] });
